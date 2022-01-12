f8dbbf907112127c3454b9df541d98dd8b033b2e
Khushi Mattu <khushimattu10@gmail.com>
2022-01-12 21:04:41 +0530

    source code

---
+++ project/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi my name is Khushi Mattu, my registration number is 20BCE1189 and this is my first app</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
