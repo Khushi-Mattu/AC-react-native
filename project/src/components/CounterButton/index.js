c1520e6c9ff2628b1a751a99a40436e7f21622b7
Khushi Mattu <khushimattu10@gmail.com>
2022-01-25 12:31:24 +0530

    Counter Application 1st look

---
+++ project/src/components/CounterButton/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import {Text,Button,StyleSheet} from 'react-native';
+const CounterButton=() =>{
+  const [count, setCount] = useState(0);
+  return (
+      <>
+
+      <Button title=" Click me each time you drink a glass of water!" color='deeppink' fontSize='20' onPress={() => setCount(count + 100)} />
+      <Text style={styles.text}>The water count is now {count} ml!</Text>
+      {/* </Button> */}
+      </>
+  );
+}
+
+const styles = StyleSheet.create({
+    text:{
+        marginTop:20,
+        marginBottom:30,
+        color:'crimson',
+        fontSize:25,
+        backgroundColor:'dodgerblue',
+        
+    }
+})
+export default CounterButton;
--- project/src/screens/CounterScreen/index.js
+++ project/src/screens/CounterScreen/index.js
@@ -1,23 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View ,Image,TextInput} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import { Dimensions } from "react-native";
+const win = Dimensions.get('window');
+import CounterButton from '../../components/CounterButton';
 
 const CounterScreen = () => {
     return (
       <View style={styles.steps}>
-       <FontAwesomeIcon icon="fa-thin fa-person-walking" size={40}/>
+      <Text style={styles.text}> Drinking water is very important!</Text>
+      <Text style={styles.text}> So, here's a counter to keep your  water intake in check!</Text>
+       <Image source={require('../../../assets/bottle.png')} style={styles.backgroundImage}></Image>
+       <CounterButton />
       </View>
     );
   }
-  
 
   const styles = StyleSheet.create({
       steps:{
-        backgroundColor: 'blue',
+        flex:1,
+        backgroundColor: 'dodgerblue',
+        justifyContent: 'space-around',
+        backgroundColor:'dodgerblue'
+        //alignItems: 'center'
+      },
+      backgroundImage:{
+        //display:flex,
+        marginTop:40,
+        marginBottom:40,
+        width: win.width/2,
+    height: (0.75)*win.width,
+    resizeMode: "contain",
+    alignSelf: "center",
+    borderWidth: 1,
+    borderRadius: 20,
+        justifyContent: 'center',
         alignItems: 'center',
-      justifyContent: 'center'
+        //height:100
+        
+      },
+      text:{
+        fontSize:25,
+        color:'indigo',
+        //backgroundColor:'deepskyblue',
+        marginTop:30,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      button:{
+        backgroundColor:'pink',
+        
       }
   })
   export default CounterScreen;
